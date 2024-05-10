452b3110a4f45b5f6302bfe746b1bda0b1b168d0
joseulisses065 <sulisses111@gmail.com>
2024-05-09 21:28:34 -0300

    task: add UserDTO

---
+++ src/main/java/com/aula/worshopmongo/dto/UserDto.java
@@ -0,0 +1,43 @@
+package com.aula.worshopmongo.dto;
+
+import com.aula.worshopmongo.domain.User;
+
+public class UserDto {
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDto() {
+    }
+
+    public UserDto(User obj) {
+        this.id = obj.getId();
+        this.email = obj.getEmail();
+        this.name = obj.getName();
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/aula/worshopmongo/resources/UserResource.java
+++ src/main/java/com/aula/worshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.aula.worshopmongo.resources;
 
 import com.aula.worshopmongo.domain.User;
+import com.aula.worshopmongo.dto.UserDto;
 import com.aula.worshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,8 +21,9 @@ public class UserResource {
     @Autowired
     private UserService service;
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDto>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> dtoList = list.stream().map(x->new UserDto(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtoList);
     }
 }
